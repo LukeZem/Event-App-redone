a78f283a8815382abdbf47e096a1cafad4a1ef68
LukeZem <lucaszemlin@outlook.com>
2023-11-01 10:32:41 -0700

    removed env log from config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 let connectionString = `mongodb+srv://lucaszemlin:${process.env.MONGO_PASS}@cluster0.eglicmo.mongodb.net/test?retryWrites=true&w=majority`
-console.log(connectionString);
+// console.log(connectionString);
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
