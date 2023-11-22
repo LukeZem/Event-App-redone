8db372194fc07684ba0c73ccc6711d1b2b34eb65
LukeZem <102622914+LukeZem@users.noreply.github.com>
2023-11-22 09:34:31 -0800

    added the pages folder back

---
+++ client/src/pages/AddPerson/index.jsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+const AddPerson = ({ setPersons }) => {
+
+    const [person, setPerson] = useState({
+        name: "",
+        role: ""
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPerson((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+
+    const handleSubmit = async (e) => {
+        console.log("form submitted");
+        e.preventDefault();
+        try {
+            const response = await axios({
+                method: "POST",
+                url: '/server/persons', // make route then fill this.,
+                data: person // person data goes here
+            })
+            console.log(response);
+            if (response.status >= 200 && response.status < 300) {
+                console.log("person added to DB", response.data);
+                setPersons(prevPersons => [...prevPersons, response.data]);
+            } else {
+                console.error("Error adding person", response.data)
+            }
+        } catch (err) {
+            console.error('Error sending request', err)
+        }
+        setPerson({
+            name: "",
+            role: ""
+        })
+    }
+
+
+    return (
+        <div>
+            <h1>Create an person</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={person.name}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="title">Role:</label>
+                    <input
+                        type="text"
+                        id="role"
+                        name="role"
+                        value={person.role}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button type='submit'>Add person</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddPerson
---
+++ client/src/pages/ViewPeople/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './index.css'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const ViewPeople = ({ people, setPeople }) => {
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios('/server/people');
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Error fetching people:', error);
+      }
+    };
+    fetchPeople();
+  }, [])
+
+
+
+  return (
+    <div className="event-list">
+      <h1>My List Of Events</h1>
+      {people.map((person) => {
+        return (
+          <div key={person._id}>
+            <h3>{person.name}</h3>
+            <p>{person.role}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ViewPeople
